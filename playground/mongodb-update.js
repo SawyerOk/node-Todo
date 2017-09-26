1c6a2d7db1baba27487c80ac021e2d0c568a263c
Alexander Yampolski <yampolal@gmail.com>
2017-09-27 01:48:19 +0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,51 @@
+const  {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+//    db.collection('Todos')
+//     .findOneAndUpdate(
+//         {
+//             _id : new ObjectID("59cace48c35b3b28387c24b1")
+//         },
+//         {
+//             $set : {
+//                 completed : true
+//             }
+//         },
+//         {
+//             returnOriginal : false
+//         }
+//     )
+//     .then((result)=>{
+//         console.log(result);
+//     });
+
+    db.collection('Users')
+        .findOneAndUpdate(
+            { 
+                _id : new ObjectID("59ca873f91addec1b2536a36")
+            },
+            { 
+                $set : {
+                    name : 'Kenny'
+                },
+                $inc : 
+                {
+                    age : 1
+                }
+            },
+            {
+                returnOriginal : false
+            }
+        )
+        .then((result)=>{
+            console.log(result);
+        })
+        
+    // db.close(); 
+});
