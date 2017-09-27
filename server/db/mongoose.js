1b3e32a3db2295b816a227e50285cf1c20dc3d9a
Alexander Yampolski <yampolal@gmail.com>
2017-09-27 22:05:00 +0300

    second

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,31 @@
+// var mongoose = require('mongoose');
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+// module.exports = {mongoose};
+
+// mongoose 4.3.x
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+/* 
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for 
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+
+var mongodbUri = 'mongodb://SLink:S123L@ds151014.mlab.com:51014/mydb';
+
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;             
+
+conn.on('error', console.error.bind(console, 'connection error:'));  
+
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
 
 module.exports = {mongoose};
